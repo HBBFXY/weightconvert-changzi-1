cdfb37e8f54c9e6c950ef45b222b7df6a1ef321e
changzi-1 <2656748923@qq.com>
2025-09-10 20:37:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # WeightConvert.py
 weight_str=input("请输入带有单位的重量值:")
-  unit=weight_str[-2].lower()
-if unit=='kg':
+  unit=weight_str[-2:].lower()
+if unit == 'kg':
   kg=float(weight_str[:-2])
   pd=kg*2.2046
   print(f"转换后的重量是{pd:.3f} pd")
-elif unit=='pd':
+elif unit == 'pd':
   pd=float(weight_str[:-2])
   kg=pd/2.2046
   print(f"转换后的重量是{kg:.3f} kg")
